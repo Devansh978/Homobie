74933adb8d1816912e2edc4c4417c11a2cdfd492
Devansh Chourey <100680201+Devansh978@users.noreply.github.com>
2025-09-17 14:40:00 +0530

    Update header.tsx

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -42,7 +42,7 @@ const navData = [
 ];
 
 
-const partnerLoginUrl = "https://homobie-frontend-portal-bco8-devansh978s-projects.vercel.app";
+const basePartnerUrl = "https://homobie-frontend-portal-bco8-devansh978s-projects.vercel.app";
 
 const partnerRoles = ["Builder", "Broker", "User", "Telecaller"];
 
